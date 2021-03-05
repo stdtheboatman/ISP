04dbcb4f80d0438d255a37d574d6d822acf29796
stdtheboatman <75591411+stdtheboatman@users.noreply.github.com>
2021-03-05 18:41:23 +0300

    Add files via upload

---
+++ Labs/Second/Task3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace task3
+{
+    class Program
+    {
+        static Int64 Solve(Int64 r)
+        {
+            Int64 result = 0;
+            while(r != 0)
+            {
+                result += r / 2;
+                r /= 2;
+            }
+
+            return result;
+        }
+
+        static Int64 Solve(Int64 a, Int64 b)
+        {
+            return Solve(b) - Solve(a - 1);
+        }
+
+        static Int64 BruteSolve(Int64 a, Int64 b)
+        {
+            BigInteger number = new BigInteger(1);
+            for(Int64 i = a; i <= b; i++)
+            {
+                number *= i;
+            }
+
+            Int64 result = 0;
+            while(number % 2 == 0)
+            {
+                result++;
+                number /= 2;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Int64[] input = Reader.ReadIntArray(2);
+
+            Int64 a = input[0];
+            Int64 b = input[1];
+
+            if (a > b)
+            {
+                Int64 temp = a;
+                a = b;
+                b = temp;
+            }
+
+            Console.WriteLine(Solve(a, b));
+            Console.WriteLine(BruteSolve(a, b));
+        }
+    }
+}
---
+++ Labs/Second/Task3/Reader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task3
+{
+    public class Reader
+    {
+        public static Int64 ReadInt64()
+        {
+            while (true)
+            {
+                string text = Console.ReadLine();
+                Int64 number;
+                if (Int64.TryParse(text, out number))
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.WriteLine("Input isn`t correct, try again");
+                }
+            }
+        }
+
+        public static Int64[] ReadIntArray(int size)
+        {
+            while (true)
+            {
+                string text = Console.ReadLine();
+                string[] array = text.Split(' ');
+                Int64[] result = new Int64[size];
+
+                if (array.Length != size)
+                {
+                    Console.WriteLine("Input isn`t correct, try again");
+                    continue;
+                }
+
+                bool isCorrectParsed = true;
+
+                for (int index = 0; index < size; index++)
+                {
+                    if (!Int64.TryParse(array[index], out result[index]))
+                    {
+                        isCorrectParsed = false;
+                        break;
+                    }
+                }
+
+                if (isCorrectParsed)
+                {
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine("Input isn`t correct, try again");
+                }
+            }
+        }
+}
+}
