ca1bd61e8bf991da0c09bb1b5664447eb3a5e1a4
stdtheboatman <75591411+stdtheboatman@users.noreply.github.com>
2021-03-05 18:18:49 +0300

    Add files via upload

---
+++ Labs/Second/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static string ReverseWordOrder(string text)
+        {
+            string[] words = text.Split(" ");
+            Array.Reverse(words);
+
+            return string.Join(" ", words);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string");
+            string s = Console.ReadLine();
+
+            s = ReverseWordOrder(s);
+            Console.WriteLine(s);
+        }
+    }
+}
