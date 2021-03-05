3e9aebe26b7eb1496f460c6857003e99373af337
stdtheboatman <75591411+stdtheboatman@users.noreply.github.com>
2021-03-05 18:20:28 +0300

    Add files via upload

---
+++ Labs/Second/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static string GetRandomString()
+        {
+            Random rnd = new Random();
+
+            int strSize = rnd.Next(1, 5);
+            string str = "";
+
+            for(int i = 0; i < strSize; i++)
+            {
+                str += (char)('a' + rnd.Next(0, 25));
+            }
+
+            return str;
+
+        }
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 16; i++)
+            {
+                Console.WriteLine(GetRandomString());
+            }
+        }
+    }
+}
