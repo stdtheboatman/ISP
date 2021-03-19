0e08b8e8683d7bd2fc55e35e7348dc13a6473bf3
stdtheboatman <75591411+stdtheboatman@users.noreply.github.com>
2021-03-19 19:08:31 +0300

    Add files via upload

---
+++ Labs/Third/Item.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace lab3
+{
+    public class Item
+    {
+        
+        public Item(string name, string description, int weight)
+        {
+            this.name = name;
+            this.description = description;
+            this.weight = weight;
+
+            this.id = objCount++;
+        }
+
+        public static void MakeSomething()
+        {
+            Console.WriteLine($"Было создано {objCount} object Item");
+        }
+
+        public override string ToString()
+        {
+            return $"name: {name}\n" +
+                   $"description: {description}\n" +
+                   $"weight: {weight}\n" +
+                   $"id: {id}" +
+                   $"\n";
+        }
+
+        public static int objCount { get; private set; }
+        
+        public int id { get; }
+        public string name { get; }
+        public string description { get; }
+        public int weight { get; }
+        
+        
+    }
+}
